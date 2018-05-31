0ed2c34217c710d0438a439a28f741897a452a2e
lherbut <lucjan.herbut@gmail.com>
2018-05-31 15:19:19 +0200

    -Added images -Color of text changes during writting Login and Password

---
+++ src/sample/controller/LoginController.java
@@ -0,0 +1,46 @@
+package sample.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField loginUsername;
+
+    @FXML
+    private JFXPasswordField loginPassword;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    void initialize() {
+
+        loginUsername.setStyle("-fx-text-inner-color: #a0a2ab");
+        loginPassword.setStyle("-fx-text-inner-color: #a0a2ab");
+
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Login was clicked ");
+            }
+        });
+    }
+
+}
+
+
