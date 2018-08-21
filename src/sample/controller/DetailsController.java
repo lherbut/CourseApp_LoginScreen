5a820fcb8cf9a2d543997cda901b96fb615bce23
lherbut <lucjan.herbut@gmail.com>
2018-08-21 20:14:03 +0200

    -Transition to the second screen after enter a login and password

--- src/sample/controller/DetailsController.java
+++ src/sample/controller/DetailsController.java
@@ -1,4 +1,43 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class DetailsController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label detailsWelcomeLabel;
+
+    @FXML
+    private Label detailsName;
+
+    @FXML
+    private Label detailsAge;
+
+    @FXML
+    private Label detailsProfession;
+
+    @FXML
+    private Label detailsLinked;
+
+    @FXML
+    private Label detailsFacebook;
+
+    @FXML
+    private Label detailsGit;
+
+    @FXML
+    void initialize() {
+
+
+
+    }
 }
--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -6,7 +6,12 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,15 +35,39 @@ public class LoginController {
     @FXML
     void initialize() {
 
-        loginUsername.setStyle("-fx-text-inner-color: #a0a2ab");
-        loginPassword.setStyle("-fx-text-inner-color: #a0a2ab");
-
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Login was clicked ");
-            }
+            public void handle(ActionEvent event){
+                loginUser();
+                }
         });
+    }
+
+
+    private void loginUser(){
+        //get main window
+
+        loginButton.getScene().getWindow().hide();
+
+        if (!loginUsername.getText().toString().trim().equals("")
+                && !loginPassword.getText().toString().trim().equals(""))
+                {
+                    Stage detailsStage = new Stage();
+                    try {
+                        FXMLLoader loader = new FXMLLoader();
+
+                        Parent root = loader.load(getClass().getResource("/sample/view/details.fxml"));
+
+                         Scene scene = new Scene(root);
+                        detailsStage.setScene(scene);
+                        detailsStage.show();
+                        detailsStage.setResizable(false);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
     }
 
 }
